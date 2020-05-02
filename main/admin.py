0769c5efadf54d282536621c0d1b7569f040ab55
Patryk <patryk.kontewicz@poczta.fm>
2020-05-02 21:18:40 +0200

    Create Group and CompanyProfile models

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from . models import Profile
+from . models import Profile, CompanyProfile, Group
 
 admin.site.register(Profile)
+admin.site.register(CompanyProfile)
+admin.site.register(Group)
--- main/models.py
+++ main/models.py
@@ -4,6 +4,31 @@ from django.conf import settings
 from datetime import datetime
 
 
+
+class CompanyProfile(models.Model):
+    company_name = models.CharField(max_length=250)
+    address_street = models.CharField(max_length=250)
+    address_number = models.CharField(max_length=150)
+    address_city = models.CharField(max_length=250)
+    address_zip = models.CharField(max_length=15)
+    company_email = models.EmailField
+    company_website = models.CharField(max_length=250)
+    company_phone = models.CharField(max_length=20)
+    company_phone2 = models.CharField(max_length=20)
+    company_nip = models.IntegerField
+    company_regon = models.IntegerField
+    is_active = models.BooleanField
+
+    def __str__(self):
+        return(self.company_name)
+
+class Group(models.Model):
+    profile = models.ForeignKey(CompanyProfile, related_name="group", on_delete=models.CASCADE)
+    group_name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return(self.group_name)
+
 class Profile(models.Model):
     user = models.ForeignKey(
         User, related_name="profile", on_delete=models.CASCADE)
