eb7a912a868c8c20cda9300c8a2b7225a45b39cd
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2020-09-19 22:32:32 +0200

    Add daschboard features in drivers page

--- react/src/components/layout/drivers/Drivers.js
+++ react/src/components/layout/drivers/Drivers.js
@@ -5,6 +5,11 @@ export class Drivers extends Component {
     return (
       <div>
         <h1>Drivers</h1>
+        <h4>Driving License</h4>
+        <h4>Tacho card</h4>
+        <h4>Employment contract</h4>
+        <h4>Medical examinations</h4>
+        <h4>Personal data</h4>
       </div>
     );
   }
