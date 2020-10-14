d148e7e4f500dba6d32fb42a4a7f03f27a0aa545
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2020-10-14 20:26:46 +0200

    Create driver model and migrate to postgres

--- drivers/models.py
+++ drivers/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Driver(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)    
+    first_name = models.CharField("Imię", max_length=50)
+    last_name = models.CharField("Nazwisko", max_length=50)
+    birth_date = models.DateField("Data urodzenia")
+    pesel = models.CharField("PESEL", max_length=11)
+    driving_license_number = models.IntegerField("Seria i numer prawa jazdy")
+    
+    
+
+    def __str__(self):
+        return (self.pesel)
