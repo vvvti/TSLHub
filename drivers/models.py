1cde22e4c940593123a0dbc60faf357e567d67f0
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2020-10-15 22:59:37 +0200

    Create DrivingLicense model in drivers app

--- drivers/models.py
+++ drivers/models.py
@@ -9,9 +9,18 @@ class Driver(models.Model):
     last_name = models.CharField("Nazwisko", max_length=50)
     birth_date = models.DateField("Data urodzenia")
     pesel = models.CharField("PESEL", max_length=11)
-    driving_license_number = models.IntegerField("Seria i numer prawa jazdy")
-    
-    
+
 
     def __str__(self):
         return (self.pesel)
+
+class DrivingLicense(models.Model):
+    
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    license_number = models.IntegerField("Numer prawa jazdy")
+    license_series = models.CharField("Seria prawa jazdy")
+    issue_date = models.DateField("Data wydania")
+    expiry_date = models.DateField("Data ważności")
+
+    def __str__(self):
+        return (self.license_number)
